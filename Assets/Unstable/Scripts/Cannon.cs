26056a105701fa5323957102e9041ee3ee9fe9c1
Daniele Cortesi <dani.c93@hotmail.it>
2021-10-02 18:07:04 +0200

    Shoot player with cannon

---
+++ Assets/Unstable/Scripts/Cannon.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Zenject;
+
+namespace Unstable
+{
+    [DisallowMultipleComponent]
+    public class Cannon : MonoBehaviour
+    {
+        #region Inspector
+
+        public float maxPlayerDistanceSquared = 25;
+
+        public float turnLerp = 0.2f;
+
+        public float dotToShoot = 0.95f;
+
+        public float secondsBetweenShots = 4;
+
+        public float force = 100;
+
+        public float yOffset = 0.5f;
+
+        public float playerSpeedMultiplier = 0;
+
+        public Rigidbody projectilePrefab;
+
+        [Header("Internals")]
+
+        [SerializeField]
+        private Transform projectileAnchor = null;
+
+        #endregion
+
+        private DateTime lastShot;
+
+        [Inject]
+        private Player player = null;
+
+        private void Start()
+        {
+            lastShot = DateTime.Now - TimeSpan.FromSeconds(secondsBetweenShots + 1);
+        }
+
+        private void Update()
+        {
+            Vector3 offset = player.transform.TransformVector(Vector3.up) * yOffset;
+
+            Vector3 targetPosition = player.transform.position + offset + player.transform.forward * player.Speed * playerSpeedMultiplier;
+
+            Vector3 toTarget = targetPosition - transform.position;
+
+            if (toTarget.sqrMagnitude > maxPlayerDistanceSquared)
+                return;
+
+            Vector3 directionToTarget = toTarget.normalized;
+
+            Vector3 updatedForward = Vector3.Lerp(transform.forward, directionToTarget, turnLerp);
+            transform.forward = updatedForward;
+
+            if ((DateTime.Now - lastShot).TotalSeconds < secondsBetweenShots)
+                return;
+
+            float dot = Vector3.Dot(updatedForward, directionToTarget);
+
+            if (dot >= dotToShoot)
+            {
+                var projectile = Instantiate(projectilePrefab, projectileAnchor.position, projectileAnchor.rotation, transform.parent);
+                projectile.AddForce(projectileAnchor.forward * force, ForceMode.Impulse);
+                lastShot = DateTime.Now;
+            }
+        }
+    }
+    
+    [Serializable]
+    public class UnityEventCannon : UnityEvent<Cannon> { }
+}
