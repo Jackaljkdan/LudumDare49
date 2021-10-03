f4ced7445920072a35c3d61eb04d410a7b2aee10
Daniele Cortesi <dani.c93@hotmail.it>
2021-10-03 08:05:03 +0200

    Add tutorial

---
+++ Assets/Unstable/Scripts/Tutorial.cs
@@ -0,0 +1,66 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Unstable.UI;
+using Zenject;
+
+namespace Unstable
+{
+    [DisallowMultipleComponent]
+    public class Tutorial : MonoBehaviour
+    {
+        #region Inspector
+
+
+
+        #endregion
+
+        [Inject]
+        private Balance balance = null;
+
+        [Inject]
+        private Player player = null;
+
+        [Inject]
+        private IntroTitles introTitles = null;
+
+        [Inject(Id = "balance")]
+        private CanvasGroup balanceCanvasGroup = null;
+
+        private void Awake()
+        {
+            balance.enabled = false;
+            player.enabled = false;
+
+            if (player.TryGetComponent(out Accelerator accelerator))
+                accelerator.enabled = false;
+
+            introTitles.onFaded.AddListener(OnIntroFading);
+        }
+
+        private void OnIntroFading()
+        {
+            player.enabled = true;
+            StartCoroutine(BalanceTutorialCoroutine());
+        }
+
+        private IEnumerator BalanceTutorialCoroutine()
+        {
+            yield return new WaitForSeconds(1);
+            yield return balanceCanvasGroup.DOFade(1, duration: 0.3f).WaitForCompletion();
+
+            yield return new WaitForSeconds(2);
+            balance.enabled = true;
+
+            yield return new WaitForSeconds(5);
+            yield return balanceCanvasGroup.DOFade(0, duration: 0.3f).WaitForCompletion();
+            balanceCanvasGroup.gameObject.SetActive(false);
+        }
+    }
+    
+    [Serializable]
+    public class UnityEventTutorial : UnityEvent<Tutorial> { }
+}
---
+++ Assets/Unstable/Scripts/UI/IntroTitles.cs
@@ -0,0 +1,53 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Zenject;
+
+namespace Unstable.UI
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(CanvasGroup))]
+    public class IntroTitles : MonoBehaviour
+    {
+        #region Inspector
+
+        [SerializeField]
+        private float fadeSeconds = 1;
+
+        public UnityEvent onFaded = new UnityEvent();
+
+        #endregion
+
+        [Inject(Id = "music")]
+        private AudioSource music = null;
+
+        [Inject]
+        private void Inject()
+        {
+            music.playOnAwake = false;
+            music.Stop();
+        }
+
+        private void Update()
+        {
+            if (UnityEngine.Input.anyKeyDown)
+            {
+                StartCoroutine(FadeCoroutine());
+                enabled = false;
+            }
+        }
+
+        private IEnumerator FadeCoroutine()
+        {
+            yield return GetComponent<CanvasGroup>().DOFade(0, duration: fadeSeconds).WaitForCompletion();
+            gameObject.SetActive(false);
+
+            music.Play();
+
+            onFaded.Invoke();
+        }
+    }
+}
